9e74d0b7f03c9a266eb56abe638871b030d20248
Rahul Yedida <yrahul3910@gmail.com>
2020-08-10 17:56:31 -0400

    Visual updates

--- src/research_interests.jsx
+++ src/research_interests.jsx
@@ -6,9 +6,12 @@ export default [
         info: (
             <div>
                 <p>I work at the intersection of artificial intelligence and software engineering.</p>
-                <p><b>Recent papers:</b></p><br/>
-                <a href="https://arxiv.org/abs/2006.00444">Recognizing actionable static code warnings using SVMs</a><br/>
-                <a href="">Improving deep learning for defect prediction</a>
+                <p><b>Recent papers:</b></p>
+                <ul>
+                    <li><a href="https://arxiv.org/abs/2006.00444">Recognizing actionable static code warnings using SVMs</a></li>
+                    <li><a href="">Improving deep learning for defect prediction</a></li>
+                </ul>
+                <br/>
             </div>
         )
     },
@@ -17,9 +20,11 @@ export default [
         info: (
             <div>
                 <p>I work on theoretical and applied deep learning, especially with loss functions.</p>
-                <p><b>Recent papers:</b></p><br/>
-                <a href="https://arxiv.org/abs/1902.07399">Adaptive learning rates for deep learning</a>
-                <a href="https://arxiv.org/abs/1906.01975">Novel activation function</a>
+                <p><b>Recent papers:</b></p>
+                <ul>
+                    <li><a href="https://arxiv.org/abs/1902.07399">Adaptive learning rates for deep learning</a></li>
+                    <li><a href="https://arxiv.org/abs/1906.01975">Novel activation function</a></li>
+                </ul>
             </div>
         )
     }

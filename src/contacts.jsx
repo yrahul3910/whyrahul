3f007478c0286202842361d92e9625d50aef7bfd
Rahul Yedida <rahul.yedida@pm.me>
2022-03-15 18:40:47 -0400

    Changed layout

--- src/contacts.jsx
+++ src/contacts.jsx
@@ -5,11 +5,7 @@ export default [
         title: "Direct contact",
         info: (
             <div>
-                <div>
-                    <i className="fab fa-skype"></i>
-                    y.rahul@outlook.com
-                </div>
-                <a href="mailto:r.yedida@pm.me">
+                <a href="mailto:ranjani125@gmail.com">
                     <i className="fas fa-envelope"></i>
                     E-mail
                 </a>
@@ -22,37 +18,23 @@ export default [
         info: (
             <div>
                 <div>
-                    <a href="https://www.github.com/yrahul3910">
+                    <a href="https://www.github.com/ranjani125">
                         <i className="fab fa-github"></i>
                         GitHub
                     </a>
                 </div>
                 <div>
-                    <a href="https://scholar.google.com/citations?user=0lP2AvkAAAAJ&hl=en">
+                    <a href="https://scholar.google.com/citations?user=d7HqEBcAAAAJ&hl=en">
                         <i className="fab fa-google"></i>
                         Google Scholar
                     </a>
                 </div>
                 <div>                    
-                    <a href="https://www.linkedin.com/in/rahul-yedida/">
+                    <a href="https://www.linkedin.com/in/ranjani-iyer/">
                         <i className="fab fa-linkedin-in"></i>
                         LinkedIn
                     </a>
                 </div>
-            </div>
-        )
-    },
-    {
-        title: "Social",
-        info: (
-            <div>
-                <div>
-                    <a href="https://twitter.com/rahulyedida13">
-                        <i className="fab fa-twitter"></i>
-                        @rahulyedida13
-                    </a>
-                </div>
-                
             </div>
         )
     }

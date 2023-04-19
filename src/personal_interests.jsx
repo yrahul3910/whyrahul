e244e6e3de78f8af461509790cc4664b0aee6909
Rahul Yedida <rahul.yedida@pm.me>
2023-04-19 14:37:43 -0400

    Added lazy loading to images

--- src/personal_interests.jsx
+++ src/personal_interests.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default [{
     title: "Coffee",
     info: <>
-        <img style={{ width: "80%", height: "auto" }} src="/coffee.png" />
+        <img style={{ width: "80%", height: "auto" }} loading="lazy" src="/coffee.png" />
         <p>
             I am a coffee enthusiast. Currently, I brew my pour overs using the Orea v3, and my espresso using
             the Breville Barista Express and a Sette 270Wi. My favorite coffee so far was a Panama Geisha from
@@ -13,7 +13,7 @@ export default [{
 }, {
     title: "Taylor Swift",
     info: <>
-        <img style={{ width: "80%", height: "auto" }} src="/taylor.jpeg" />
+        <img style={{ width: "80%", height: "auto" }} loading="lazy" src="/taylor.jpeg" />
         <p>
             I&apos;ve been a big fan of Taylor Swift since 2018! My favorite era is the Lover era, although I currently have 
             Midnights on repeat. My current top 5 favorite songs (in no particular order) are <i>Hits Different,</i>
@@ -24,7 +24,7 @@ export default [{
 }, {
     title: "Smart Home",
     info: <>
-        <img style={{ width: "80%", height: "auto" }} src="/smart_home.jpeg" />
+        <img style={{ width: "80%", height: "auto" }} loading="lazy" src="/smart_home.jpeg" />
         <p>
             As fragmented as the smart home market can be, I am slowly getting deeper into it in the HomeKit ecosystem.
             My current favorite use of the smart home is to remotely turn on my espresso machine using the SwitchBot Bot

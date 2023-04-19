1891c6a1e253c2ef7c82d313cbd16f0739b9d666
Rahul Yedida <rahul.yedida@pm.me>
2023-04-19 15:34:24 -0400

    Bug fix in responsive image

--- components/NewBanner.jsx
+++ components/NewBanner.jsx
@@ -11,8 +11,6 @@ export default class NewBanner extends React.Component {
                         sizes="(max-width: 400px) 400px, (max-width: 600px) 600px, (max-width: 800px) 800px, (max-width: 1000px) 1000px, (max-width: 1200px) 1200px, (max-width: 1600px) 1600px, (min-width: 1601px) 2000px"
                         src="profile.jpg"
                         alt=""
-                        width="2153"
-                        height="3327"
                     />
                 </div>
                 <div className="nav">

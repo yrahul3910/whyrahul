063d103aee778fb23f1f4163a94d323e1f9738df
Rahul Yedida <rahul.yedida@pm.me>
2023-07-08 06:35:24 -0400

    Updated favorite songs

--- src/personal_interests.jsx
+++ src/personal_interests.jsx
@@ -15,10 +15,11 @@ export default [{
     info: <>
         <img style={{ width: "80%", height: "auto" }} loading="lazy" src="/taylor.jpeg" />
         <p>
-            I&apos;ve been a big fan of Taylor Swift since 2018! My favorite era is the Lover era, although I currently have 
-            Midnights on repeat. My current top 5 favorite songs (in no particular order) are <i>Hits Different,</i>
+            I&apos;ve been a big fan of Taylor Swift since 2018! My favorite era is the Lover era, although I currently have
+            Midnights on repeat. My current favorite songs (in no particular order) are <i>Hits Different,</i>
             <i>Cruel Summer,</i> <i>All Too Well (10 Minute Version) [Taylor&apos; Version] (From The Vault),</i>
-            <i>Anti-Hero,</i> and <i>You&apos;re On Your Own, Kid,</i> although this list frequently changes.
+            <i>I Can See You (Taylor&apos; Version) (From The Vault),</i> <i>You&apos;re On Your Own, Kid,</i>
+            <i>Foolish One (Taylor&apos; Version) (From The Vault),</i> although this list frequently changes.
         </p>
     </>
 }, {

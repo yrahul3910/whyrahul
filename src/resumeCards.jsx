ad9023a29037bad318ba5d1e68f289e7a218e602
Rahul Yedida <rahul.yedida@pm.me>
2024-03-01 10:43:00 -0500

    Added faculty application statements

--- src/resumeCards.jsx
+++ src/resumeCards.jsx
@@ -7,7 +7,16 @@ export default [
             <div>
                 <p>Most of my publications are in SE venues, since my research is at the intersection of deep learning and software engineering. My academic CV is below.</p>
                 <p>
-                    <a href="https://github.com/yrahul3910/resume/raw/master/pdf/academic.pdf">Download</a>
+                    <a href="https://github.com/yrahul3910/resume/raw/master/pdf/academic.pdf">CV</a>
+                </p>
+                <p>
+                    <a href="https://github.com/yrahul3910/whyrahul/raw/master/assets/research_statement.pdf">Research Statement</a>
+                </p>
+                <p>
+                    <a href="https://github.com/yrahul3910/whyrahul/raw/master/assets/teaching_statement.pdf">Teaching Statement</a>
+                </p>
+                <p>
+                    <a href="https://github.com/yrahul3910/whyrahul/raw/master/assets/diversity_statement.pdf">Diversity Statement</a>
                 </p>
             </div>
     },
